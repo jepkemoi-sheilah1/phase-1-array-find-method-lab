be78c5fdf5df72d0d9780f38a1f07f1a9feaafe3
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-11 20:47:48 +0300

    create array find() to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+     { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+   // { year: "2014", result: "N/A"},
+   //{ year: "2013", result: "L"},
+
+]
+function superbowlWin( record){
+   // console.log("Record:", record);
+   const win = record.find(game => game.result.trim() === "W");
+
+   // const win =record.find( game => game.result==="w");
+   // console.log("Win Object:", win); // Debugging line to check what `find()` returns
+    return win?win.year : undefined;
+    
+}
+console.log(superbowlWin(record));
